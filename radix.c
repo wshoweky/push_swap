a872f9a46504e534c0d092cb821554bed45af50f
wshoweky <wshoweky@student.hive.fi>
2025-07-15 15:53:51 +0300

    Update radix.c

--- radix.c
+++ radix.c
@@ -1,48 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   radix.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshoweky <wshoweky@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 11:59:43 by wshoweky          #+#    #+#             */
+/*   Updated: 2025/07/15 15:53:18 by wshoweky         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 static int	get_max_bits(t_list **stack)
 {
-	t_list	*head;
-	int		max;
+	t_list	*node;
+	int		max_index;
 	int		max_bits;
 
-	head = *stack;
-	max = head->index;
+	node = *stack;
+	max_index = node->index;
 	max_bits = 0;
-	while (head)
+	while (node)
 	{
-		if (head->index > max)
-			max = head->index;
-		head = head->next;
+		if (node->index > max_index)
+			max_index = node->index;
+		node = node->next;
 	}
-	while ((max >> max_bits) != 0)
+	while (max_index >> max_bits)
 		max_bits++;
 	return (max_bits);
 }
 
 void	radix_sort(t_list **stack_a, t_list **stack_b)
 {
-	t_list	*head_a;
+	t_list	*node;
 	int		i;
-	int		j;
+	int		n;
 	int		size;
 	int		max_bits;
 
 	i = 0;
-	head_a = *stack_a;
-	size = ft_lstsize(head_a);
+	node = *stack_a;
+	size = ft_lstsize(node);
 	max_bits = get_max_bits(stack_a);
 	while (i < max_bits)
 	{
-		j = 0;
-		while (j++ < size)
+		n = 0;
+		while (n++ < size)
 		{
-			if (((head_a->index >> i) & 1) == 1)
+			if ((node->index >> i) & 1)
 				ra(stack_a);
 			else
-				pb(stack_a, stack_b);
+				pb(stack_b, stack_a);
 		}
-		while (ft_lstsize(*stack_b) != 0)
+		while (ft_lstsize(*stack_b))
 			pa(stack_a, stack_b);
 		i++;
 	}
