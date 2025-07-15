bbb55c82e9d5d61ac61df05e3752f1926de341b4
wshoweky <wshoweky@student.hive.fi>
2025-07-15 15:55:44 +0300

    Update rotate.c

--- rotate.c
+++ rotate.c
@@ -1,43 +1,40 @@
 #include "push_swap.h"
 
-// Shift up all elements of a stack by 1. The first element becomes the last one | ra and rb
-int	rotate(t_list **stack)
+int	rotate(t_list	**stack)
 {
-	t_list	*head;
-	t_list	*tail;
+	t_list	*first;
+	t_list	*last;
 
 	if (ft_lstsize(*stack) < 2)
-		return (-1);
-	head = *stack;
-	tail = ft_lstlast(head);
-	*stack = head->next;
-	head->next = NULL;
-	tail->next = head;
-	return (0);	
+		return (0);
+	first = *stack;
+	last = ft_lstlast(*stack);
+	*stack = (*stack)->next;
+	last->next = first;
+	first->next = 0;
+	return (1);
 }
 
 int	ra(t_list **stack_a)
 {
-	if (rotate(stack_a) == -1)
-		return (-1);
+	if (!rotate(stack_a))
+		return (0);
 	ft_putendl_fd("ra", 1);
-	return (0);
+	return (1);
 }
 
 int	rb(t_list **stack_b)
 {
-	if (rotate(stack_b) == -1)
-		return (-1);
+	if (!rotate(stack_b))
+		return (0);
 	ft_putendl_fd("rb", 1);
-	return (0);
+	return (1);
 }
 
 int	rr(t_list **stack_a, t_list **stack_b)
 {
-	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
-		return (-1);
-	rotate(stack_a);
-	rotate(stack_b);
+	if (!(rotate(stack_a)) || !(rotate(stack_b)))
+		return (0);
 	ft_putendl_fd("rr", 1);
-	return (0);
+	return (1);
 }
