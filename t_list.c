4979bfb52b1534da573d65ab5146c1251f268627
wshoweky <wshoweky@student.hive.fi>
2025-07-15 15:58:42 +0300

    Update t_list.c

--- t_list.c
+++ t_list.c
@@ -1,71 +1,44 @@
 #include "push_swap.h"
 
-/*
-// Creates new node and returns the pointer of it
-t_list	*ft_lstnew(int value)
+t_list	*ft_lstlast(t_list *node)
 {
-	t_list	*new;
-
-	new = (t_list *) malloc(sizeof(*new));
-	if (!new)
-		return (NULL);
-	new->value = value;
-	new->index = -1;
-	new->next = NULL;
-	return (new);
-}
-// Adds the specified node to a stack (list) making it the head
-void	ft_lstadd_front(t_list **stack, t_list *new)
-{
-	new->next = *stack;
-	*stack = new;
-}
-*/
-// Returns the last node of a list 
-t_list	*ft_lstlast(t_list *head)
-{
-	t_list	*tmp;
-
-	tmp = head;
-	while (tmp->next)
+	if (!node)
+		return (0);
+	while (node->next)
 	{
-		tmp = tmp->next;
-		if (tmp->next == NULL)
-			return (tmp);
+		node = node->next;
+		if (!(node->next))
+			return (node);
 	}
-	return (tmp);
+	return (0);
 }
 
-// Adds the specified node to a stack (list) making it the last node
-void	ft_lstadd_back(t_list **stack, t_list *new)
+void	ft_lstadd_back(t_list **stack, t_list *node)
 {
-	t_list	*n;
+	t_list	*last;
 
 	if (*stack)
 	{
-		n = ft_lstlast(*stack);
-		n->next = new;
-		new->next = NULL;
+		last = ft_lstlast(*stack);
+		last->next = node;
+		node->next = 0;
 	}
 	else
 	{
-		*stack = new;
-		(*stack)->next = NULL;
+		*stack = node;
+		node->next = 0;
 	}
 }
 
-// Returns the size of the Linked List
-int	ft_lstsize(t_list *head)
+int	ft_lstsize(t_list *node)
 {
-	size_t	i;
-	t_list	*tmp;
+	int	n;
 
-	tmp = head;
-	i = 0;
-	while (tmp)
+	n = 0;
+	while (node)
 	{
-		tmp = tmp->next;
-		i++;
+		n++;
+		node = node->next;
 	}
-	return (i);
+	return (n);
 }
