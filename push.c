5c77909f86d0b7cb776684f4b3314b4d592f72cc
wshoweky <wshoweky@student.hive.fi>
2025-07-17 18:17:31 +0300

    Update push.c

--- push.c
+++ push.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshoweky <wshoweky@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 16:16:54 by wshoweky          #+#    #+#             */
+/*   Updated: 2025/07/17 16:16:56 by wshoweky         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-// Takes the first element of one stack and puts it at the top of another | pa and pb
 int	push(t_list **stack_to, t_list **stack_from)
 {
 	t_list	*tmp;
 	t_list	*head_to;
 	t_list	*head_from;
 
+	if (!stack_from || !*stack_from)
+		return (-1);
 	if (ft_lstsize(*stack_from) == 0)
 		return (-1);
 	head_to = *stack_to;
@@ -36,7 +49,7 @@ int	pa(t_list **stack_a, t_list **stack_b)
 	return (0);
 }
 
-int	pb(t_list **stack_a, t_list **stack_b)
+int	pb(t_list **stack_b, t_list **stack_a)
 {
 	if (push(stack_b, stack_a) == -1)
 		return (-1);
