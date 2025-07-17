fc6b7459dc5dcb187541abd4caea347ab0284415
wshoweky <wshoweky@student.hive.fi>
2025-07-17 18:20:49 +0300

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshoweky <wshoweky@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 16:22:25 by wshoweky          #+#    #+#             */
+/*   Updated: 2025/07/17 16:22:27 by wshoweky         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 int	swap(t_list **stack)
@@ -7,12 +19,15 @@ int	swap(t_list **stack)
 	int		tmpv;
 	int		tmpi;
 
-	if (ft_lstsize(*stack) < 2)
+	if (!stack || !*stack || ft_lstsize(*stack) < 2)
 		return (-1);
 	node = *stack;
 	next = node->next;
 	if (!node || !next)
+	{
+		stack_free (stack);
 		ft_error("Error");
+	}
 	tmpv = node->value;
 	tmpi = node->index;
 	node->value = next->value;
