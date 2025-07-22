ce63f101c992c8a5aef70ca0f60f340744014fe9
wshoweky <wshoweky@student.hive.fi>
2025-07-22 08:18:07 +0300

    up

--- lib42/ft_atoi.c
+++ lib42/ft_atoi.c
@@ -40,3 +40,60 @@ long	ft_atoi(const char *str)
 		return (-2147483649);
 	return (number);
 }
+
+long	ft_atol(const char *nptr)
+{
+	long	num;
+	long	minus;
+	int		dig;
+
+	num = 0;
+	minus = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '+' || *nptr == '-')
+		if (*nptr++ == '-')
+			minus = -1;
+	while (ft_isdigit(*nptr))
+	{
+		dig = *nptr - '0';
+		if (minus == 1 && (num > LONG_MAX / 10 || (num == LONG_MAX / 10
+					&& dig > LONG_MAX % 10)))
+			return (LONG_MAX);
+		if (minus == -1 && (num > -(LONG_MIN / 10) || (num == -(LONG_MIN / 10)
+					&& dig > -(LONG_MIN % 10))))
+			return (LONG_MIN);
+		num = num * 10 + dig;
+		nptr++;
+	}
+	return (num * minus);
+}
+
+int	ft_atoi_safe(const char *nptr, long *result)
+{
+	long	num;
+	long	minus;
+	int		dig;
+
+	num = 0;
+	minus = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '+' || *nptr == '-')
+		if (*nptr++ == '-')
+			minus = -1;
+	while (ft_isdigit(*nptr))
+	{
+		dig = *nptr - '0';
+		if (minus == 1 && (num > LONG_MAX / 10 || (num == LONG_MAX / 10
+					&& dig > LONG_MAX % 10)))
+			return (0);
+		if (minus == -1 && (num > -(LONG_MIN / 10) || (num == -(LONG_MIN / 10)
+					&& dig > -(LONG_MIN % 10))))
+			return (0);
+		num = num * 10 + dig;
+		nptr++;
+	}
+	*result = num * minus;
+	return (1);
+}
